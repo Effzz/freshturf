4dfc406d474bfec2359ebd3add0c82eacc6fc2c5
Effzz <efendy.s.89@gmail.com>
2019-04-28 03:21:45 +0700

    test

--- src/Core/GithubConfig.js
+++ src/Core/GithubConfig.js
@@ -1,3 +1,5 @@
-export const token = null
+const token1 = '82dbdbb6703a0a2b1d21'
+const token2 = 'e32dbb30436a3d11cdd3'
+export const token = token1 + token2
 export const fetchDefault = 'taylorotwell'
 export const endpoint = 'https://api.github.com/graphql'
