3a371c5ebb3d39c8de8edf5a85d1a84922707a62
Efendy Salim <efendy.s.89@gmail.com>
2019-04-28 02:42:29 +0700

    Update GithubConfig.js

--- src/Core/GithubConfig.js
+++ src/Core/GithubConfig.js
@@ -1,3 +1,3 @@
-export const token = '2259aac054d32a0e2afa36f09a4861f72bdb1138'
+export const token = '621ea02cc2aaaedca4e38f475e45d43f16dd4195'
 export const fetchDefault = 'taylorotwell'
 export const endpoint = 'https://api.github.com/graphql'
