8066d734c007f65ec6c97e5d56df8b689e619836
Effzz <efendy.s.89@gmail.com>
2019-04-28 03:26:13 +0700

    update#2

--- src/Core/GithubConfig.js
+++ src/Core/GithubConfig.js
@@ -1,5 +1,5 @@
-const token1 = '82dbdbb6703a0a2b1d21'
-const token2 = 'e32dbb30436a3d11cdd3'
+const token1 = '40a8ed48b60fb73cdc70'
+const token2 = '1026c2786c71e672193d'
 export const token = token1 + token2
 export const fetchDefault = 'taylorotwell'
 export const endpoint = 'https://api.github.com/graphql'
